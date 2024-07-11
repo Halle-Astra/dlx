79edf687009d77029b0450a7dbb462308413d639
halle-astra <12535232+halle-astra@user.noreply.gitee.com>
2024-07-12 01:35:27 +0800

    balabala

--- data/datasets/nlp/wudao.py
+++ data/datasets/nlp/wudao.py
@@ -3,3 +3,8 @@ class WuDao:
     def __init__(self, root):
         pass
 
+    def process_each_item(self, item):
+        # dataType, title, content,
+        # id 虽然有用，但是id感觉没法一次性全部读入的情况下，还是蛮难用的。
+        pass
+
