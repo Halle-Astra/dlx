daccecf4edc824569fa9e4e43db1185a84d8213f
Halle-Astra <halle.wang@qq.com>
2024-07-12 20:22:36 +0800

    write few lines

--- data/datasets/nlp/wudao.py
+++ data/datasets/nlp/wudao.py
@@ -1,7 +1,25 @@
 # my own root: /media/halle/GTA/data/llms/WuDaoCorpus2.0_base_200G
+import os
+import json
+import glob
+from threading import Thread
+from multiprocessing import Process
+
+
+class Dataset:
+    def __init__(self, num_workers=8):
+        self.nw = num_workers
+
+    def init_processes(self):
+        self.processes = []
+
+
 class WuDao:
-    def __init__(self, root):
-        pass
+    def __init__(self, root, change_file_iters=2000):
+        self.files = glob.glob(os.path.join(root, '*.json'))
+        self.current_file = None
+        self.content_list = None
+
 
     def process_each_item(self, item):
         # dataType, title, content,
