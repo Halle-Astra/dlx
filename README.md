31264aacb398abbd222893823ca334145475065e
Halle-Astra <halle.wang@qq.com>
2024-07-16 19:16:08 +0800

    fix the bugs of mp

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 https://cloud.tencent.com/developer/article/1836076
 
 https://developer.ibm.com/data/project-codenet/#get-this-dataset1
+
+目前总结，terminate容易造成孤儿进程等一系列问题，应当使用event等来让程序正常结束
+
+取数据要一个线程
--- data/datasets/nlp/wudao.py
+++ data/datasets/nlp/wudao.py
@@ -21,43 +21,46 @@ def wf(*args):
     while True:
         worker_func(*args)
 
-def worker_func(contents_num, content_list, queue, process_count, lock):  # 进程
-    try:
-        logger.info('contents_num: {}, process_count: {}'.format(contents_num.value,
-                                                                 process_count.value))
-        if contents_num.value == 0:
-            return 
-        sample_ind = random.choice(range(contents_num.value))
-        sample = content_list[sample_ind]
-        title = sample['title']
-        content = sample['content']
-        text = '\n'.join([title, content])
-        queue.put(text)
-        lock.acquire()
-        process_count.value += 1
 
-        lock.release()
-    except Exception as e:
-        logger.error(str(e))
+def worker_func(contents_num, content_list, queue, process_count, lock, workers_exit):  # 进程
+    while not workers_exit.is_set():
+        try:
+            logger.info('contents_num: {}, process_count: {}, data_queue_size:{}'.format(
+                contents_num.value,
+                process_count.value,
+                queue.qsize()
+            ))
+
+            if contents_num.value == 0:
+                return
+            sample_ind = random.choice(range(contents_num.value))
+            sample = content_list[sample_ind]
+            title = sample['title']
+            content = sample['content']
+            text = '\n'.join([title, content])
+            queue.put(text)
+            lock.acquire()
+            process_count.value += 1
+            lock.release()
+        except Exception as e:
+            logger.error(str(e))
 
 
-class WorkerManager(Thread):
+class WorkerWatcher(Thread):
     def __init__(self, dataset_instance, num_proc=8):
         super().__init__()
         self.ds = dataset_instance
 
     def run(self, *args, ):
         while True:
-            if self.ds.debug:
-                try :
-                    self.ds.data_queue.get(timeout=5)
-                except Exception as e:
-                    logger.debug(e)
-            logger.debug(f'current process_count: {self.ds.process_count.value}, data_q_size: {self.ds.data_queue.qsize()}')
-            if self.ds.current_file is None or (self.ds.process_count.value > self.ds.change_file_iters):
+
+            # do this finally
+            logger.debug(
+                f'current process_count: {self.ds.process_count.value}, data_q_size: {self.ds.data_queue.qsize()}')
+            if self.ds.current_file is None or (self.ds.process_count.value % self.ds.change_file_iters == 0):
                 self.ds.rload_file()
                 self.ds.arrange_workers()
-            time.sleep(10)
+            # time.sleep(10)
 
 
 class WuDao:
@@ -72,24 +75,62 @@ class WuDao:
         self.content_list = list()
         self.contents_num = Value('i', 0)
         self.data_queue = Queue(maxsize=queue_size)
-        self.worker_manager = WorkerManager(self,8)
+        self.worker_manager = WorkerWatcher(self, 8)
         self.process_count = Value('i', 0)
         self.change_file_event = Event()
+        self.workers_exit_event = Event()
 
-        self.worker_manager.start()#self.process_count.value)
+        self.worker_manager.start()  # self.process_count.value)
         # self.rload_file()
 
         self.workers = None
 
+        self.queue2list_thread = Thread(
+            target=self.queue2list,
+            args=(self,)
+        )
+        self.queue2list_thread.start()
+
+    def queue2list(self, dataset_instance):
+        while True:
+            if dataset_instance.debug:
+                # try :
+                if dataset_instance.data_queue.qsize() > 0:
+                    # logger.info("取数据中, empty:{}, qsize:{}".format(
+                    #     dataset_instance.data_queue.empty(),
+                    #     dataset_instance.data_queue.qsize()
+                    # ))
+                    time1 = time.time()
+                    dataset_instance.data_queue.get()
+                    time2 = time.time()
+                    logger.info("time cost of data taking: {}".format(time2 - time1))
+                # except Exception as e:
+                #     logger.debug(e)
+
     def arrange_workers(self):
+        try:
+            self.lock.release()
+            logger.info("锁已释放")
+        except Exception as e:
+            logger.warning("锁无法释放或早已释放")
+            logger.error(str(e))
+
+        self.workers_exit_event.set()
         if self.workers is not None:
-            for w in self.workers:
-                # w.join()
-                w.terminate()
-                w.close()
+            while True:
+                exit_num = 0
+                for w in self.workers:
+                    if not w.is_alive():
+                        exit_num += 1
+                if exit_num == len(self.workers):
+                    logger.info("所有进程已成功退出")
+                    break
+        self.workers_exit_event.clear()
+
         self.workers = [Process(
-            target=wf,
-            args=(self.contents_num, self.content_list, self.data_queue, self.process_count, self.lock)
+            target=worker_func,
+            args=(self.contents_num, self.content_list, self.data_queue, self.process_count, self.lock,
+                  self.workers_exit_event)
         ) for i in range(8)]
         for w in self.workers:
             w.start()
@@ -109,13 +150,6 @@ class WuDao:
         self.change_file_event.set()
         logger.debug('Source file is loaded.')
 
-    def worker_func(self):
-        pass
-
-    def process_each_item(self, item):
-        # dataType, title, content,
-        # id 虽然有用，但是id感觉没法一次性全部读入的情况下，还是蛮难用的。
-        pass
 
 if __name__ == "__main__":
     root = '/dataset/fd5061f6/chinese_data/WuDao/'
