9817f5337c816b3775d4e8e9c8d5990e7e53b44f
Halle-Astra <halle.wang@qq.com>
2024-07-25 21:16:19 +0800

    revise lamabda x: x into kaiming_init_method

--- experiments/llama3/pretrain.py
+++ experiments/llama3/pretrain.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
     #     te_weight = {'tok_embeddings.weight': weights}
     model = Transformer(margs).cuda()
     # model.load_state_dict(weights)
-    model = DDP(model)
+    model = DDP(model, retain_graph=True)
 
     # others
     optimizer = Adam(model.parameters(), lr=1e-5)
