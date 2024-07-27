1dfa6d857a895ed0b4588b800249b04bfae67602
精东 王 <halle.wang@qq.com>
2024-07-28 05:36:12 +0800

    same problem from old argument name

--- utils/data/nlp/file_segments_dataloader.py
+++ utils/data/nlp/file_segments_dataloader.py
@@ -97,7 +97,7 @@ class FileSegmentsDataloader(Dataloader):
             #     f'current process_count: {self.dataloader.process_count.value}, data_q_size: {self.dataloader.data_queue.qsize()}')
             if self.current_file is None or \
                     (self.process_count.value // self.change_file_iters > \
-                     self.dataloader.change_file_times.value):
+                     self.change_file_times.value):
                 self.rload_file()
                 self.arrange_workers()
         logger.warning("WorkerWatcher is exiting.")
