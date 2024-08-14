0fb24650892d981d52a6b515ab4fd98e74de9cf9
精东 王 <halle.wang@qq.com>
2024-08-15 05:31:35 +0800

    modify the init of self.change_file_event

--- utils/data/nlp/file_segments_dataloader.py
+++ utils/data/nlp/file_segments_dataloader.py
@@ -39,7 +39,6 @@ class FileSegmentsDataloader(Dataloader):
 
         # variables for changing file
         self.process_count = Value('i', 0)
-        self.change_file_event = self.workers_exit_event  # Event()
 
         # This init method will start the worker watcher which need the self.current_file has been initialized.
         if hasattr(dataset_instance, 'collate_fn'):
@@ -54,6 +53,7 @@ class FileSegmentsDataloader(Dataloader):
             collate_fn=collate_fn,
             **kwargs
         )
+        self.change_file_event = self.workers_exit_event  # Event()
 
     def rload_file(self):
         self.current_file = random.choice(self.files)
