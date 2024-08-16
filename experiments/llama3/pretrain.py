386d702d18b0ee07dc385b4e4db52860207889cd
Halle-Astra <halle.wang@qq.com>
2024-08-16 19:23:16 +0800

    support amp

--- experiments/llama3/pretrain.py
+++ experiments/llama3/pretrain.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
         optimizer=optimizer,
         world_size=2, tokenizer=tokenizer,
         model_is_kv_cache_enabled=False,
-        dtype=torch.long, parallel='ddp',
+        ids_dtype=torch.long, parallel='ddp',
         grad_clip=None, device='cuda',
         amp=True
     )
